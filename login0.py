ae93608d45988c2c24f61dd5b23f96d2e867548c
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2021-07-10 22:19:49 -0600

    rand gen 2nd error fix

--- login0.py
+++ login0.py
@@ -15,7 +15,7 @@ def getip():
 
 
 def newsessionid():
-    sessionid=''.join(random.choices(string.ascii_letters + string.digits,24))
+    sessionid=''.join(random.choices(string.ascii_letters + string.digits,k=24))
     return sessionid
 
 
